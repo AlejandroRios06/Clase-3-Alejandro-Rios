1b4c369c8bff009e81aa5a98eddc70ec1d2bfd71
AlejandroRios06 <alejandro.rios.06@hotmail.com>
2021-10-02 17:27:26 -0300

    Initial commit

---
+++ archivo.js
@@ -0,0 +1,8 @@
+
+let numero = Number(prompt("Veamos la multiplicaciones. ingrese un numero."))
+
+for (let i = 1; i <= 10 ; i++) {
+    
+    console.log (`${numero} X ${i} = ${numero*i}`)
+    
+}
